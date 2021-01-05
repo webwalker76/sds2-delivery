44c57291354705fc3417762fed0de12b19cb7627
ASUS <alexscosta76@gmail.com>
2021-01-05 09:37:38 -0300

    Project created

---
+++ backend/src/main/java/com/dsdelivery/sistema/entregas/SistemaEntregasApplication.java
@@ -0,0 +1,13 @@
+package com.dsdelivery.sistema.entregas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEntregasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEntregasApplication.class, args);
+	}
+
+}

a34d3ef3bb0ea365ad6ced068a89118271163ccb
ASUS <alexscosta76@gmail.com>
2021-01-10 13:16:11 -0300

    add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { OrderPayLoad } from "./Orders/types";
 
 const mapboxToken=process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
-const API_URL = 'http://localhost:8080';
+const API_URL =  process.env.REACT_APP_API_URL;
 
 export function fetchProducts(){
     // eslint-disable-next-line no-template-curly-in-string
